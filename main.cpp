b7ae829f734b24cb3141e78b25b9f0581fe9a5f4
Color-Lemon <2983899551@qq.com>
2021-07-04 15:08:28 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
+	int tmp = 0;
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0; j < size - i - 1; j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
     
 }
 
 int main()
 {
     int arr[] = {12, 4, 89, 43, 21, 78};
-    int size = sizeot(arr)/sizeof(arr[0]);
+    int size = sizeof(arr)/sizeof(arr[0]);
     sort(arr,size);
 
-    return o;
+    return 0;
 }
